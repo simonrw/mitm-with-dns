8827c8b6e2594b4790ca50641e884a0a393f345c
Simon Walker <simon.walker@localstack.cloud>
2023-02-05 20:50:38 +0000

    Add certificate updating

--- cmd/init/main.go
+++ cmd/init/main.go
@@ -1,11 +1,59 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
+	"io/ioutil"
+	"os"
+	"strings"
 	"time"
 )
 
+func splitLines(s string) []string {
+	var lines []string
+
+	sc := bufio.NewScanner(strings.NewReader(s))
+	for sc.Scan() {
+		lines = append(lines, sc.Text())
+	}
+
+	return lines
+}
+
 func main() {
 	fmt.Println("Init process")
+
+	// copy the ca certificate
+	// TODO: non-platform specific
+	certs, err := ioutil.ReadFile("/etc/ssl/certs/ca-certificates.crt")
+	if err != nil {
+		panic(err)
+	}
+	certLines := splitLines(string(certs))
+
+	rootCa, err := ioutil.ReadFile("/customcerts/ca/rootCA.pem")
+	if err != nil {
+		panic(err)
+	}
+	caLines := splitLines(string(rootCa))
+
+	var allLines []string
+	for _, line := range certLines {
+		allLines = append(allLines, line)
+	}
+	for _, line := range caLines {
+		allLines = append(allLines, line)
+	}
+
+	// clobber output file
+	f, err := os.Create("/etc/ssl/certs/ca-certificates.crt")
+	if err != nil {
+		panic(err)
+	}
+	if _, err := f.WriteString(strings.Join(allLines, "\n")); err != nil {
+		panic(err)
+	}
+	fmt.Println("certificates updated")
+
 	time.Sleep(86400 * time.Second)
 }
--- internal/docker/docker.go
+++ internal/docker/docker.go
@@ -46,18 +46,39 @@ func copyFile(src, dst string) error {
 	return nil
 }
 
+func copyFiles(srcs, dsts []string) error {
+	if len(srcs) != len(dsts) {
+		panic("mismatch between sources and destinations")
+	}
+	n := len(srcs)
+	for i := 0; i < n; i++ {
+		err := copyFile(srcs[i], dsts[i])
+		if err != nil {
+			return fmt.Errorf("copying %s to %s: %w", srcs[i], dsts[i], err)
+		}
+	}
+	return nil
+}
+
 func (c dockerClient) buildImage(ctx context.Context, name, base string) error {
 	logger.Debug().Str("name", name).Str("base", base).Msg("building image")
 	dockerfileContents := fmt.Sprintf(`
 	FROM %s
 	COPY init /init
 	RUN chmod +x /init
+	RUN mkdir -p /customcerts/ca
+	COPY *.pem /customcerts/
+	COPY ca/*.pem customcerts/ca/
 	ENTRYPOINT ["/init"]
 	`, base)
 
 	contextDir, err := os.MkdirTemp("", "dockerbuild-*")
 	if err != nil {
 		return fmt.Errorf("creating temp dir: %w", err)
+	}
+
+	if err := os.Mkdir(filepath.Join(contextDir, "ca"), 0o777); err != nil {
+		return fmt.Errorf("creating ca dir: %w", err)
 	}
 
 	filename := filepath.Join(contextDir, "Dockerfile")
@@ -75,9 +96,24 @@ func (c dockerClient) buildImage(ctx context.Context, name, base string) error {
 
 	logger.Debug().Str("filename", file.Name()).Msg("")
 
-	// copy the init biext: %w", err)nary
+	// copy the init binary
 	if err := copyFile("./init", filepath.Join(contextDir, "init")); err != nil {
 		return fmt.Errorf("copying init binary into context: %w", err)
+	}
+
+	// copy the certificates
+	if err := copyFiles([]string{
+		"./_wildcard.amazonaws.com-key.pem",
+		"./_wildcard.amazonaws.com.pem",
+		"./ca/rootCA-key.pem",
+		"./ca/rootCA.pem",
+	}, []string{
+		filepath.Join(contextDir, "_wildcard.amazonaws.com-key.pem"),
+		filepath.Join(contextDir, "_wildcard.amazonaws.com.pem"),
+		filepath.Join(contextDir, "ca", "rootCA-key.pem"),
+		filepath.Join(contextDir, "ca", "rootCA.pem"),
+	}); err != nil {
+		return err
 	}
 
 	// tar up the docker build into a context
@@ -127,7 +163,7 @@ func Run(stop chan struct{}, complete chan struct{}) {
 
 	res, err := cli.ContainerCreate(ctx, &container.Config{
 		Image: "foo",
-	}, nil, nil, nil, "")
+	}, nil, nil, nil, "container")
 	if err != nil {
 		logger.Fatal().Err(err).Msg("could not create container")
 	}
@@ -135,13 +171,27 @@ func Run(stop chan struct{}, complete chan struct{}) {
 		logger.Fatal().Err(err).Msg("could not start container")
 	}
 
+	containerRemove := func() {
+		logger.Info().Msg("removing container")
+		// remove the container
+		if err := cli.ContainerRemove(ctx, res.ID, types.ContainerRemoveOptions{Force: true}); err != nil {
+			logger.Warn().Err(err).Msg("failed to remove container")
+		}
+	}
+
 	statusCh, errCh := cli.ContainerWait(ctx, res.ID, container.WaitConditionNotRunning)
 	select {
+	case <-stop:
+		containerRemove()
+		complete <- struct{}{}
+		return
 	case err := <-errCh:
 		if err != nil {
+			containerRemove()
 			logger.Fatal().Err(err).Msg("error running container")
 		}
 	case <-statusCh:
+		containerRemove()
 	}
 
 	out, err := cli.ContainerLogs(ctx, res.ID, types.ContainerLogsOptions{ShowStdout: true})
